782e1dfca0296ca823e2a88e05dc853560c7c578
Armine <Armine@LAPTOP-CO4RGELN>
2020-04-23 18:21:23 +0400

    last changes

---
+++ Homework 01/Homework 01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    *      * * *  *        *  *  *     *  *  *  *");
+            Console.Write("   * *     *   *  *  *  *  *  *  * *   *  *      \n");
+            Console.WriteLine("  * * *    * * *  *    *   *  *  *  *  *  *  *  *");
+            Console.WriteLine(" *     *   * *    *        *  *  *   * *  *      ");
+            Console.WriteLine("*       *  *   *  *        *  *  *     *  *  *  *");
+        }
+    }
+}
