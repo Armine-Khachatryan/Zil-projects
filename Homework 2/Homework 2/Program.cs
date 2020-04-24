a66e4f7237cd2a3711d134a2c9931b2ef7546b37
Armine <Armine@LAPTOP-CO4RGELN>
2020-04-24 21:21:09 +0400

    Homework 2

---
+++ Homework 2/Homework 2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string m = "57";
+            string n = "13";
+            string all1 = m + n;
+            Console.WriteLine(all1);
+            short y = 6;
+            short x = 10;
+            int all = y + x;
+            Console.WriteLine(all);
+            int c = y - x;
+            Console.WriteLine(c);
+            int k = y * x;
+            Console.WriteLine(k);
+            int j = y % x;
+            Console.WriteLine(j);
+            int i1 = 26;
+            int i2 = 20;
+            int i3 = i1 + i2;
+            Console.WriteLine(i3);
+            int i4 = i1 - i2;
+            Console.WriteLine(i4);
+            int i5 = i1 * i2;
+            Console.WriteLine(i5);
+            int i6 = i1 / i2;
+            Console.WriteLine(i6);
+            long l1 = 135;
+            long l2 = 230;
+            long l3 = l1 + l2;
+            Console.WriteLine(l3);
+            long l4 = l1 - l2;
+            Console.WriteLine(l4);
+            long l5 = l1 * l2;
+            Console.WriteLine(l5);
+            long l6 = l1 / l2;
+            Console.WriteLine(l6);
+            float f1 = 5.3f;
+            float f2 = 3.6f;
+            float f3 = f1 + f2;
+            Console.WriteLine(f3);
+            float f4 = f1 - f2;
+            Console.WriteLine(f4);
+            float f5 = f1 * f2;
+            Console.WriteLine(f5);
+            float f6 = f1 / f2;
+            Console.WriteLine(f6);
+            double d1 = 233.6;
+            double d2 = 156.9;
+            double d3 = d1 + d2;
+            Console.WriteLine(d3);
+            double d4 = d1 - d2;
+            Console.WriteLine(d4);
+            double d5 = d1 * d2;
+            Console.WriteLine(d5);
+            double d6 = d1 / d2;
+            Console.WriteLine(d6);
+            decimal D1 = 15556.3m;
+            decimal D2 = 16666.2m;
+            decimal D3 = D1 + D2;
+            Console.WriteLine(D3);
+            decimal D4 = D1 - D2;
+            Console.WriteLine(D4);
+            decimal D5 = D1 * D2;
+            Console.WriteLine(D5);
+            decimal D6 = D1 / D2;
+            Console.WriteLine(D6);
+            byte b1 = 26;
+            byte b2 = 20;
+            int b3 = b1 + b2;
+            Console.WriteLine(b3);
+        }
+    }
+}
