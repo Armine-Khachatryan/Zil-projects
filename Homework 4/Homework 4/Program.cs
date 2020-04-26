b7cd4cad9c164660a6ac9a145ffc6b04a73fa1de
Armine <Armine@LAPTOP-CO4RGELN>
2020-04-26 17:23:15 +0400

    last changes

---
+++ Homework 4/Homework 4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(15 & 34 & 26);
+            Console.WriteLine(15 | 34 | 26);
+            Console.WriteLine(15 ^ 34 ^ 26);
+            Console.WriteLine(15 >> 34 >> 26);
+            Console.WriteLine(15 << 34 << 26);
+        }
+    }
+}
