ef2d6df034848cf86e4f1472306f95c48bbaa825
Armine-Khachatryan <arminehkhachatryan@gmail.com>
2020-05-03 15:44:31 +0400

    last change

---
+++ Homework 7/Homework 7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                sum += digit;
+                number /= 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
+
