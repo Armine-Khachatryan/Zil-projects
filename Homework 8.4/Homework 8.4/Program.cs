7ec7d89de0006e8525295074001e674aea1c5020
Armine-Khachatryan <arminehkhachatryan@gmail.com>
2020-05-03 18:50:15 +0400

    last change

---
+++ Homework 8.4/Homework 8.4/Program.cs
@@ -0,0 +1,21 @@
+﻿//task 4
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_8._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(Math.Pow(n, i));
+            }
+        }
+    }
+}
