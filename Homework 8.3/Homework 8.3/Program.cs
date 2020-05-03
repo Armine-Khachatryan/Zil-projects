fab5144f86ea5832d3d1404c46b92ccfdcb95cf1
Armine-Khachatryan <arminehkhachatryan@gmail.com>
2020-05-03 17:10:43 +0400

    last changes

---
+++ Homework 8.3/Homework 8.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_8._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i=0; i<n+1; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
