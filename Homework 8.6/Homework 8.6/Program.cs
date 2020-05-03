c5ca01fb8b58cfd61fb8b52da6c1bf2ef567a246
Armine-Khachatryan <arminehkhachatryan@gmail.com>
2020-05-03 20:11:11 +0400

    last changes

---
+++ Homework 8.6/Homework 8.6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_8._6
+{
+    class Program
+    {
+        static void Main()
+        {
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            for (int i = A; i <= B; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(B - A + 1);
+        }
+    }
+}
