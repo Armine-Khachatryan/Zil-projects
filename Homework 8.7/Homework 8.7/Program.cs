a1ba59b688b64d1727bfb871da58e9650bb48955
Armine-Khachatryan <arminehkhachatryan@gmail.com>
2020-05-03 20:25:31 +0400

    last changes

---
+++ Homework 8.7/Homework 8.7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_8._6
+{
+    class Program
+    {
+        static void Main()
+        {
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            if (A < B) 
+            {
+                int sum = 0;
+                for (int i = A; i <= B; i++)
+                {
+                    sum += i;
+                }
+                Console.WriteLine(sum);
+            }
+            else
+            {
+                Console.WriteLine("wrong");
+            }
+
+
+        }
+    }
+}
