dc740b9e38e06fbfcb91d7b665beb72e92b0dd82
Armine-Khachatryan <arminehkhachatryan@gmail.com>
2020-05-03 19:25:28 +0400

    last changes

---
+++ Homework 8.5/Homework 8.5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_8._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < N; i+=2)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
