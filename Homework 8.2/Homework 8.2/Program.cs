f8dc157ae3941dc2c9fbcddbfcc70908a06a4d27
Armine-Khachatryan <arminehkhachatryan@gmail.com>
2020-05-03 18:24:46 +0400

    last changes

---
+++ Homework 8.2/Homework 8.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int K = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(K);
+            }
+           
+        }
+    }
+}
