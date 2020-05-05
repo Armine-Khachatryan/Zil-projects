83caca047eb2dffa47c8eb6a6b6d7bba6b13f1b1
Armine_Khachatryan <arminehkhachatryan@gmail.com>
2020-05-05 17:28:04 +0400

    last changes

---
+++ Homework 8.8/Homework 8.8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_8._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            if (A<B)
+            {
+                for (int i = A; i <= B; i++)
+                {
+                    if (i % 3 == 0) 
+                    {
+                        Console.WriteLine(i);
+                        break;
+                    }     
+                }
+            }
+            else
+            {
+                Console.WriteLine("nothing");
+            }
+        }
+    }
+}
---
+++ Homework 8.9/Homework 8.9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_8._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            double sum = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                sum = sum + (double)1/i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
